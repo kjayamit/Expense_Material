ac3eac545d066b1d232b09659c25b8145043707a
Jaya Chakravarthi Kuppu <jayachk@thoughtworks.com>
2017-05-01 21:51:44 +0530

    added espresso test

---
+++ src/androidTest/java/com/example/android/materialdesigncodelab/MainActivityTest.java
@@ -0,0 +1,126 @@
+package com.example.android.materialdesigncodelab;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.assertion.ViewAssertions.*;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.my_recycler_view),
+                        withParent(allOf(withId(R.id.viewpager),
+                                withParent(withId(R.id.main_content)))),
+                        isDisplayed()));
+        recyclerView.perform(actionOnItemAtPosition(0, click()));
+
+//        ViewInteraction textView = onView(
+//                allOf(withText("Description"),
+//                        childAtPosition(
+//                                childAtPosition(
+//                                        IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class),
+//                                        0),
+//                                0),
+//                        isDisplayed()));
+//        textView.check(matches(withText("Description")));
+//
+//        ViewInteraction appCompatImageButton = onView(
+//                allOf(withContentDescription("Navigate up"),
+//                        withParent(allOf(withId(R.id.toolbar),
+//                                withParent(withId(R.id.collapsing_toolbar)))),
+//                        isDisplayed()));
+//        appCompatImageButton.perform(click());
+//
+//        ViewInteraction appCompatTextView = onView(
+//                allOf(withText("Tile"), isDisplayed()));
+//        appCompatTextView.perform(click());
+//
+//        ViewInteraction recyclerView2 = onView(
+//                allOf(withId(R.id.my_recycler_view),
+//                        withParent(allOf(withId(R.id.viewpager),
+//                                withParent(withId(R.id.main_content)))),
+//                        isDisplayed()));
+//        recyclerView2.perform(actionOnItemAtPosition(2, click()));
+//
+//        ViewInteraction textView2 = onView(
+//                allOf(withText("Location"),
+//                        childAtPosition(
+//                                childAtPosition(
+//                                        IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class),
+//                                        0),
+//                                2),
+//                        isDisplayed()));
+//        textView2.check(matches(withText("Location")));
+//
+//        ViewInteraction appCompatImageButton2 = onView(
+//                allOf(withContentDescription("Navigate up"),
+//                        withParent(allOf(withId(R.id.toolbar),
+//                                withParent(withId(R.id.collapsing_toolbar)))),
+//                        isDisplayed()));
+//        appCompatImageButton2.perform(click());
+//
+//        ViewInteraction appCompatTextView2 = onView(
+//                allOf(withText("Card"), isDisplayed()));
+//        appCompatTextView2.perform(click());
+//
+//        ViewInteraction recyclerView3 = onView(
+//                allOf(withId(R.id.my_recycler_view),
+//                        withParent(allOf(withId(R.id.viewpager),
+//                                withParent(withId(R.id.main_content)))),
+//                        isDisplayed()));
+//        recyclerView3.perform(actionOnItemAtPosition(3, click()));
+//
+//        ViewInteraction appCompatImageButton3 = onView(
+//                allOf(withContentDescription("Navigate up"),
+//                        withParent(allOf(withId(R.id.toolbar),
+//                                withParent(withId(R.id.collapsing_toolbar)))),
+//                        isDisplayed()));
+//        appCompatImageButton3.perform(click());
+//
+    }
+//
+//    private static Matcher<View> childAtPosition(
+//            final Matcher<View> parentMatcher, final int position) {
+//
+//        return new TypeSafeMatcher<View>() {
+//            @Override
+//            public void describeTo(Description description) {
+//                description.appendText("Child at position " + position + " in parent ");
+//                parentMatcher.describeTo(description);
+//            }
+//
+//            @Override
+//            public boolean matchesSafely(View view) {
+//                ViewParent parent = view.getParent();
+//                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+//                        && view.equals(((ViewGroup) parent).getChildAt(position));
+//            }
+//        };
+//    }
+}
